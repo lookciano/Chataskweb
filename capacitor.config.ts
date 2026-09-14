613ae909725f6cc4ae6ce89609d361a14d6a2405
Luciano Magalhaes <lookciano@gmail.com>
2026-09-14 14:34:33 -0300

    fix: align ios xcode signing

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "com.gste.chattask",
+  appId: "com.lookciano.chattask",
   appName: "Chat Task",
   webDir: "dist/public",
   server: {
